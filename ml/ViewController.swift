7f54dd1142a27c005a643d895e4c52f606b5283e
Karan Gandhi <kag354@rutgers.edu>
2021-04-15 10:20:07 -0400

    Initial Commit

---
+++ ml/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  ml
+//
+//  Created by Karan Gandhi on 4/15/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+    
+    
+    
+    @IBAction func cameraPressed(_ sender: UIBarButtonItem) {
+    }
+
+}
+
